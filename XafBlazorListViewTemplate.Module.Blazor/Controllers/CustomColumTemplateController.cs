6fb711be587ecac910a49f754d75b9b56126a153
Jose Manuel Ojeda Melgar <joche.ojeda@bitframeworks.com>
2022-01-13 06:55:40 -0700

    Add project files.

---
+++ XafBlazorListViewTemplate.Module.Blazor/Controllers/CustomColumTemplateController.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.Blazor;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Blazor.Editors.Grid;
+using DevExpress.ExpressApp.Blazor.Editors.Grid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XafBlazorListViewTemplate.Module.BusinessObjects;
+
+namespace XafBlazorListViewTemplate.Module.Blazor.Controllers
+{
+    public partial class CustomColumTemplateController : ObjectViewController<ListView,Person>
+    {
+        public CustomColumTemplateController()
+        {
+           
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            GridListEditor gridListEditor = (GridListEditor)View.Editor;
+          
+            IDxDataGridAdapter dataGridAdapter = gridListEditor.GetDataGridAdapter();
+
+           
+
+
+
+            foreach (DxDataGridColumnModel columnModel in dataGridAdapter.DataGridColumnModels)
+            {
+                if (columnModel.Field ==nameof(Person.Email))
+                {
+                    columnModel.DisplayTemplate = LinkColumnTemplate.Create();
+                   
+                }
+            }
+        }
+        // ...
+        //protected override void OnViewControlsCreated()
+        //{
+        //    base.OnViewControlsCreated();
+        //    if (View.Editor is GridListEditor gridListEditor)
+        //    {
+        //        IDxDataGridAdapter dataGridAdapter = gridListEditor.GetDataGridAdapter();
+        //        dataGridAdapter.DataGridModel.ColumnResizeMode = DataGridColumnResizeMode.Component;
+        //        //var columns=dataGridAdapter.ComponentContent.
+        //    }
+        //}
+        // ...
+    }
+}
